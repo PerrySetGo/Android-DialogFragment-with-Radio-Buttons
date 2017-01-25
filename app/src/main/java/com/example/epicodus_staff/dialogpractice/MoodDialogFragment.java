82fd607758088c4893cb2b09bf09151808516aaf
perry eising <perry.r.eising@gmail.com>
2017-01-24 16:28:46 -0800

    add cancel and submit buttons to dialog XML file.

--- app/src/main/java/com/example/epicodus_staff/dialogpractice/MoodDialogFragment.java
+++ app/src/main/java/com/example/epicodus_staff/dialogpractice/MoodDialogFragment.java
@@ -2,9 +2,14 @@ package com.example.epicodus_staff.dialogpractice;
 
 import android.app.DialogFragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 /**
  * Created by epicodus_staff on 1/24/17.
@@ -12,10 +17,16 @@ import android.view.ViewGroup;
 
 public class MoodDialogFragment extends DialogFragment{
 
+    RadioButton selectedRadioButton;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_mood_dialog, container, false);
-        getDialog().setTitle("Simple Dialog to test");
+        final View rootView = inflater.inflate(R.layout.fragment_mood_dialog, container, false);
+
+
+
         return rootView;
     }
+
 }
